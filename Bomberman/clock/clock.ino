c2c553781024cc583f72f971255530731aa5fcc7
ChristiaanGuliker <s1131440@student.windesheim.nl>
2019-11-15 09:50:01 +0100

    Update clock.ino
    Comment vereterd

--- Bomberman/clock/clock.ino
+++ Bomberman/clock/clock.ino
@@ -14,7 +14,7 @@ void setup() {
   TCCR2A = (1 << COM2A0) | (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
   TCCR2B = (1 << WGM22) | (1 << CS21);
 
-//  58 kHz 
+//  56 kHz 
 //  OCR2A = 36;
 //  OCR2B = (OCR2a/2);
 
