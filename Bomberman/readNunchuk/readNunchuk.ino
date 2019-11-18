23ab96f6dcea54171d7b2a98f480adb4906e0ec5
JelleAardema <ja.aardema@gmail.com>
2019-11-18 15:14:37 +0100

    added comments

--- Bomberman/readNunchuk/readNunchuk.ino
+++ Bomberman/readNunchuk/readNunchuk.ino
@@ -1,14 +1,11 @@
 #include <Wire.h>
 #include "nunchuk.h"
 
-
-
 void setup() {
     Serial.begin(9600);
     Wire.begin();
     // nunchuk_init_power(); // A1 and A2 is power supply
     nunchuk_init();
-
 
     /*
      * nunchuk_buttonZ(); 0 of 1
@@ -19,8 +16,10 @@ void setup() {
 }
 
 void loop() {
+    // check if there's any data to read.
     if (nunchuk_read()) {
-
+      
+       // deadzones of 20, so you're not moving when you're not using the stick.
        if(nunchuk_joystickX() > 20) {
           Serial.println("rechts");
        }
@@ -37,10 +36,12 @@ void loop() {
           Serial.println("links");
        }
 
+       // Z button
        if(nunchuk_buttonZ() == 1) {
           Serial.println("Z ingedrukt");
        }
 
+       // C button
        if(nunchuk_buttonC() == 1) {
           Serial.println("C ingedrukt");
        }
