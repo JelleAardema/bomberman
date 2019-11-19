1ed90493823a4c61935a1585ec326e5702cd7239
JelleAardema <ja.aardema@gmail.com>
2019-11-19 12:03:59 +0100

    converted to library

--- Bomberman/readNunchuk/readNunchuk.c
+++ Bomberman/readNunchuk/readNunchuk.c
@@ -1,51 +1,40 @@
 #include <Wire.h>
 #include "nunchuk.h"
 
-void setup() {
-    Serial.begin(9600);
-    Wire.begin();
-    // nunchuk_init_power(); // A1 and A2 is power supply
-    nunchuk_init();
-
-    /*
-     * nunchuk_buttonZ(); 0 of 1
-     * nunchuk_buttonC(); 0 of 1
-     * nunchuk_joystickX(); -126 tot 127
-     * nunchuk_joystickY(); -128 tot 126
-     */
+void setupNunchuk() {
+  nunchuk_init();
 }
 
-void loop() {
+int getInput() {
     // check if there's any data to read.
     if (nunchuk_read()) {
       
        // deadzones of 20, so you're not moving when you're not using the stick.
        if(nunchuk_joystickX() > 20) {
-          Serial.println("rechts");
+           return 1;
        }
 
        else if(nunchuk_joystickY() > 20) {
-          Serial.println("omhoog");
+           return 2;
        }
 
        else if(nunchuk_joystickY() < -20) {
-          Serial.println("beneden");
+           return 3;
        }
 
        else if(nunchuk_joystickX() < -20) {
-          Serial.println("links");
+           return 4;
        }
 
        // Z button
        if(nunchuk_buttonZ() == 1) {
-          Serial.println("Z ingedrukt");
+           return 5;
        }
 
        // C button
        if(nunchuk_buttonC() == 1) {
-          Serial.println("C ingedrukt");
+           return 6;
        }
        
     }
-    delay(10);
 }
