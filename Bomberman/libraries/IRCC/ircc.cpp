4d6e2fb6d17d46f716199568fa8f37ad1864fdc1
ChristiaanGuliker <s1131440@student.windesheim.nl>
2019-11-20 12:12:51 +0100

    prescaler 256

--- Bomberman/libraries/IRCC/ircc.cpp
+++ Bomberman/libraries/IRCC/ircc.cpp
@@ -136,7 +136,7 @@ void convertBites(){
 
 void sendIR(int data){
   sendData = data;
-  TCCR0B |= (1 << CS02) | (1 << CS00);  //enable timer0
+  TCCR0B |= (1 << CS02));  //enable timer0
   sendBit();
 }
 
@@ -150,7 +150,7 @@ void sendBit(){
     }
   }else{
     if(OCR0A == timeBreak){   //after a breaktime disable timer0
-      TCCR0B &= ~((1 << CS02) | (1 << CS00));
+      TCCR0B &= ~((1 << CS02) | (1 << (CS01) | (1 << CS00));
     }else{        //if no more data
       OCR0A = timeBreak;
     }
@@ -174,7 +174,7 @@ void timer0Init() {
 void timer1Init(){
   TCCR1A = 0;
   TCCR1B = 0;
-  TCCR1B |= (1<<CS12)|(1<<CS10);
+  TCCR1B |= (1<<CS12);
 }
 
 void timer2Init() {
