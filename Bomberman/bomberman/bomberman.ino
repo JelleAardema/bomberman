6fbfbc16da7a81f1f974c989cee9f78e2fbdef9b
wimjan <wimjan99@gmail.com>
2019-11-21 13:19:51 +0100

    inclusion of working player

--- Bomberman/bomberman/bomberman.ino
+++ Bomberman/bomberman/bomberman.ino
@@ -28,14 +28,6 @@ struct DIMENSION
   uint16_t width;
   uint16_t height;
 };
-
-// functions
-void drawGrid(Adafruit_ILI9341 *,struct DIMENSION,uint8_t grid[GRID_X][GRID_Y]); 
-void drawBlock(Adafruit_ILI9341 *,struct DIMENSION,uint8_t);
-void changeBlock(Adafruit_ILI9341 *,struct DIMENSION,uint8_t grid[GRID_X][GRID_Y],uint16_t,uint16_t,uint8_t);
-void calcBlock(struct DIMENSION screen,struct DIMENSION *block,uint16_t x,uint16_t y);
-
-// types
 enum AIM
 {
   UP, DOWN, LEFT, RIGHT
@@ -45,6 +37,16 @@ struct PLAYER
 {
   uint16_t x,y;
 };
+// functions
+void drawGrid(Adafruit_ILI9341 *,struct DIMENSION,uint8_t grid[GRID_X][GRID_Y]); 
+void drawBlock(Adafruit_ILI9341 *,struct DIMENSION,uint8_t);
+void changeBlock(Adafruit_ILI9341 *,struct DIMENSION,uint8_t grid[GRID_X][GRID_Y],uint16_t,uint16_t,uint8_t);
+void calcBlock(struct DIMENSION screen,struct DIMENSION *block,uint16_t x,uint16_t y);
+
+uint8_t step(enum AIM direction,uint8_t world[9][9],struct PLAYER p1,struct DIMENSION scren); 
+void drawPlayer(struct PLAYER p1, Adafruit_ILI9341 *pen,struct DIMENSION screen);
+// types
+
 
 // functions
 uint8_t step(enum AIM direction,uint8_t world[9][9],struct player p1);
@@ -63,14 +65,14 @@ Adafruit_ILI9341 screen = Adafruit_ILI9341(TFT_CS, TFT_DC);
       {1,1,1,1,1,1,1,1,1}};
   
 
-  struct DIMENSION dimension = {10,10,219,200};
-  
+  struct DIMENSION dimension = {10,10,220,220};
+  struct PLAYER player1 = {1,1}; 
 void setup() {
     // setup
   screen.begin(); 
 
   // zet cordinate system
-    screen.setRotation(2);  
+    screen.setRotation(1);  
 
   // draw start screen
   screen.fillScreen(0x0000);
@@ -85,4 +87,6 @@ void loop() {
     delay(1000);
     changeBlock(&screen,dimension,wrld,4,4,0);
     delay(1000);
+    drawPlayer(player1,&screen,dimension);
+     
 }
