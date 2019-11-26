9aecca098e76677ecde759d3f4ea7028fa390861
ChristiaanGuliker <s1131440@student.windesheim.nl>
2019-11-26 13:11:02 +0100

    New library Connection
    all IRCC function in one function to send/receive

--- Bomberman/testIR/White/White.ino
+++ Bomberman/testIR/White/White.ino
@@ -6,10 +6,10 @@
 
 #include <encodeData.h>
 #include <ircc.h>
+#include <connection.h>
 
 
-volatile uint16_t incomingByte = 0;// for serial port 
-volatile uint16_t receiveByte = 0;
+volatile uint16_t incomingByte = 0;// for serial port
 int type = 0;
 
 int x1 = 6;
@@ -29,35 +29,13 @@ int b2 = 0;
 int main() {
 	//setup
 	Serial.begin(9600);     //temp; to start serial monitor
-	transmitDataSetup(host);
 	Serial.println("Loaded");
+  // SEARCHING FOR OTHER ARDUINO
+  startConnection(host);
+  Serial.println("connected");
   
-	while(1) {
-		if (Serial.available() > 0) {
-			incomingByte = Serial.read();
-			sendIRCC(encodeMovement(x1,y1,l1,b1));
-		}
-		receiveByte = receiveIRCC();	
-		if(receiveByte){
-			type = decodeMessageType(receiveByte);
-			
-			Serial.print("type: ");
-			Serial.println(type,BIN);
-			
-			decodeMovement(&x2,&y2,&l2,&b2);
-			
-			Serial.print("x2:  ");
-			Serial.print(x2,DEC);
-			Serial.print("   ");
-			Serial.print("y2:  ");
-			Serial.print(y2,DEC);
-			Serial.print("   ");
-			Serial.print("l2:  ");
-			Serial.print(l2,DEC);
-			Serial.print("   ");
-			Serial.print("b2:  ");
-			Serial.println(b2,DEC);
-		}
-	}
+  while(1){
+    
+  }
   return 0;
 }
