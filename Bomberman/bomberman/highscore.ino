4cef8e2ea4d31502051599643443c43f9458673f
JelleAardema <ja.aardema@gmail.com>
2019-12-04 14:53:54 +0100

    moves highscores 1 place down

--- Bomberman/bomberman/highscore.ino
+++ Bomberman/bomberman/highscore.ino
@@ -1,26 +1,52 @@
 #include <EEPROM.h>
 
-uint16_t addr = 256;
+uint16_t addr;
 
 void setup() {
   Serial.begin(9600);
   
-  uint16_t score = 300;
-  uint16_t getScore = 0;
+  uint16_t score = 7;
 
   placeHighscore(score);
+  getHighscores();
 }
 
 void placeHighscore(uint16_t newScore) {
+
+  addr = 256;
+  
+  // Loop through the 5 old highscores
+  for(int i = 0; i < 5; i++) {
+
+    uint16_t oldScore, lowerScore;
+    EEPROM.get(addr, oldScore);
+
+    if(newScore > oldScore) {
+      // Loop through the scores that would be below the new high score
+      for(int j = 0; j < 5 - i; j++) {
+
+        // Move all the highscores 1 place down
+        EEPROM.get(addr + 16 * j, lowerScore);
+        EEPROM.put(addr + 16 + 16 * j, lowerScore);  
+      }
+
+      // Place the new highscore
+      EEPROM.put(addr, newScore);
+      break;
+    }
+
+    addr += 16;
+  }
+}
+
+void getHighscores() {
+
+  addr = 256;
+  uint16_t score;
   
   for(int i = 0; i < 5; i++) {
-    EEPROM.put(addr, newScore);
-
-    uint16_t currentScore = 0;
-    EEPROM.get(addr, currentScore);
-
-    Serial.println(currentScore);
-    
+    EEPROM.get(addr, score);
+    Serial.println(score);
     addr += 16;
   }
 }
