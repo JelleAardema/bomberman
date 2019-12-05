67fc8c7e4315988ac73b760c3a027f9cb8588552
JelleAardema <ja.aardema@gmail.com>
2019-12-05 13:21:46 +0100

    update test code

--- Bomberman/libraries/highscore/highscore.ino
+++ Bomberman/libraries/highscore/highscore.ino
@@ -3,13 +3,14 @@
 void setup() {
   Serial.begin(9600);
 
-  // Test code
+  // Place 5 scores in the EEPROM
   placeHighscore(1);
   placeHighscore(5);
   placeHighscore(4);
   placeHighscore(2);
   placeHighscore(3);
 
+  // Print highscores
   for(int i = 0; i < 5; i++) {
     Serial.println(highscores[i]);
   }
