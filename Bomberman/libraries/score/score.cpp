9ef52c4853377bec8c61daf5787b03e57d2e478d
Tiemen Molenaar <s1128667@student.windesheim.nl>
2019-12-11 13:50:32 +0100

    begin gemaakt aan score bibliotheek

---
+++ Bomberman/libraries/score/score.cpp
@@ -0,0 +1,9 @@
+#include <Adafruit_ILI9341.h>
+#include <Adafruit_GFX.h>
+#include "grid.h"
+#include "score.h"
+
+// draw the core on screen
+void drawScore(Adafruit_ILI9341 *pen,struct DIMENSIONS dimension, int score)
+{
+}
---
+++ Bomberman/libraries/score/score.h
@@ -0,0 +1,11 @@
+#include <Adafruit_ILI9341.h>
+#include <Adafruit_GFX.h>
+#include "grid.h"
+
+#ifndef SCORE_H
+#include SCORE_H
+
+// ---- functions
+void drawScore(Adafruit_ILI9341 *pen,struct DIMENSIONS dimension, int score);
+
+#endif
